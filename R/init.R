2b4c9c3288c2e9001c68c23a4c63de89e34f99ec
Jeroen Ooms <jeroenooms@gmail.com>
2019-04-30 01:29:48 +0200

    Fall back on anonymous PAT for rate limits

--- R/init.R
+++ R/init.R
@@ -2,4 +2,10 @@
   options(menu.graphics = FALSE)
   dir.create("~/.R", showWarnings = FALSE)
   file.create("~/.R/Makevars", showWarnings = FALSE)
+
+  # Fall back on anonymous PAT for rate limits
+  if(is.na(Sys.getenv('GITHUB_PAT', NA))){
+    Sys.setenv(CI = 1)
+    Sys.setenv(GITHUB_PAT = remotes:::github_pat())
+  }
 }
